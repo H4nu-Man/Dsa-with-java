69a8927c54e2877096e3b3592df721915d917613
H4nu-Man <sasanksai2120@gmail.com>
2025-11-23 22:39:50 +0530

    Add files via upload

---
+++ Sorting/Selection_sort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Selection_sort {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int arr[] = new int[n];
+
+        // Input
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        // Selection Sort
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        // Output
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
